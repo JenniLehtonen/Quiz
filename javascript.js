fed7fab03ea1a8e8c53a63806ed874fc25d34d19
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2020-12-29 17:36:57 +0200

    Add comments

--- javascript.js
+++ javascript.js
@@ -29,12 +29,12 @@ document.getElementById("tryAgain").addEventListener('click', playAgain);
     if(questionIndex > 3) { //If there is the last question, change button text to "Show the result"
       document.getElementById("newQuestionButton").innerHTML="Show the result";
     }
-    if(questions[questionIndex]!=undefined){
+    if(questions[questionIndex]!=undefined){ 
       var element1 = document.getElementById("b1");
       var element2 = document.getElementById("b2");
       var element3 = document.getElementById("b3");
       var element4 = document.getElementById("b4");
-      element1.classList.remove("correctAnswer");
+      element1.classList.remove("correctAnswer");//Remove classes "correctAnswer" and "wrongAnswer" and add the original class
       element1.classList.remove("wrongAnswer");
       element1.classList.add("optionButton");
       element2.classList.remove("correctAnswer");
@@ -74,26 +74,23 @@ document.getElementById("tryAgain").addEventListener('click', playAgain);
     if (e.target.innerText == answers[questionIndex]){
       var element = document.getElementById(e.target.id);
       element.classList.remove("optionButton");
-      element.classList.add("correctAnswer");
-      console.log("oikein");
+      element.classList.add("correctAnswer"); //if the answer is correct --> add a new class
       score++;
-      console.log(score);
     } else{
       var element = document.getElementById(e.target.id);
       element.classList.remove("optionButton");
-      element.classList.add("wrongAnswer");
-      console.log("väärin");
+      element.classList.add("wrongAnswer"); //if the answer is wrong --> add a new class
     }
   }
 
   function playAgain(){
     document.getElementById("newQuestionButton").innerHTML="New question";
-    document.getElementById("score").style.display = "none";
-    document.getElementById("tryAgain").style.display = "none";
+    document.getElementById("score").style.display = "none"; //hide the score
+    document.getElementById("tryAgain").style.display = "none"; //hide "try again" button
     score = 0;
     questionIndex = 0;
     console.log(questionIndex);
-    var x = document.getElementsByClassName("questionAndOptions");
+    var x = document.getElementsByClassName("questionAndOptions"); //show question and options again
     var i;
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "block";
@@ -103,7 +100,7 @@ document.getElementById("tryAgain").addEventListener('click', playAgain);
       var element2 = document.getElementById("b2");
       var element3 = document.getElementById("b3");
       var element4 = document.getElementById("b4");
-      element1.classList.remove("wrongAnswer");
+      element1.classList.remove("wrongAnswer"); //Remove classes "correctAnswer" and "wrongAnswer" and add the original class
       element1.classList.remove("correctAnswer");
       element1.classList.add("optionButton");
       element2.classList.remove("wrongAnswer");
@@ -117,9 +114,9 @@ document.getElementById("tryAgain").addEventListener('click', playAgain);
       element4.classList.add("optionButton");
 
     document.getElementById("questionNumber").innerHTML = "1 of " + questions.length;
-    document.getElementById("question").innerHTML=questions[0];
+    document.getElementById("question").innerHTML=questions[0]; //reset the index of the questions
 
-    document.getElementById("b1").innerHTML=options[0].a;  
+    document.getElementById("b1").innerHTML=options[0].a;  //show the first options
     document.getElementById("b2").innerHTML=options[0].b;
     document.getElementById("b3").innerHTML=options[0].c;
     document.getElementById("b4").innerHTML=options[0].d;
